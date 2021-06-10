9c9f5ad80d3493b5f774c6f7a9b6f4ace2c53399
Kenneth Fossen <fossen.kenneth@gmail.com>
2021-06-10 14:21:56 +0200

    added(Solutionfile) One Solution to rule them all

--- apiclient/Program.fs
+++ apiclient/Program.fs
@@ -1,23 +1,35 @@
 // Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
 
 open System
+open System.Net.Http
 open SwaggerProvider
 
 let [<Literal>] Schema = "https://localhost:5001/swagger/v1/swagger.json"
 type Weather = OpenApiClientProvider<Schema>
 
+// Straigth up example from https://fsprojects.github.io/SwaggerProvider/#/OpenApiClientProvider
 
 [<EntryPoint>]
 let main argv =
-    let client = Weather.Client()
+    let handler = new HttpClientHandler(UseCookies = false)
+    let baseUri = Uri("https://localhost:5001/")
+    let httpClient = new HttpClient(handler, true, BaseAddress=baseUri)
+    let client = Weather.Client(httpClient)
+    
+    printfn "WeatherData"
+    printfn "Calling Endpoint /api/WeatherForcast/Bergen"
     client.GetApiWeatherForecast1("Bergen")
         |> Async.AwaitTask
         |> Async.RunSynchronously
-        |> printf "%O"
-
+        |> Array.map(printfn "%O")
+        |> ignore
+        
+    printfn "" 
+    printfn "Calling Endpoint /api/WeatherForcast/"
     client.GetApiWeatherForecast()
         |> Async.AwaitTask
         |> Async.RunSynchronously
-        |> printfn "%O"
+        |> Array.map( printfn "%O")
+        |> ignore
 
     0 // return an integer exit code
